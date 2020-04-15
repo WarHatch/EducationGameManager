6a9e8898bec45ddb9db440b53bc13c7eba3d97f0
Karolis Staskevičius <staskeviciuskarolis@gmail.com>
2020-04-15 19:34:11 +0300

    security note

--- src/dataHandler/cms.ts
+++ src/dataHandler/cms.ts
@@ -3,6 +3,7 @@ import sanity from "@sanity/client"
 const sanityClient = sanity({
   projectId: "0byunyul",
   dataset: "teachers_content",
+  // FIXME: for security reasons pass as env variable
   token: "skeFzBaDaGhcffZb6SzShSUOpRU6JDR46Y8696X2ZoPvYjUU94Jhc6CIWO3hTgbnDYC9l0EDjB41IFWbRxTvAzuQnDqL1uqvKJX8R4CoAx2Y26iR5cY22FGkMuVtURtCOomDjoBWU7ZzwmWYD2c3YNAzubbluBzgRZuMkfYRWjabBqQnci3M",
 })
 
